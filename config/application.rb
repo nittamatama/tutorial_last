b83520e759c31b14dae23ad929b20a2e0902fec3
nittamatama <blackmnshin@gmail.com>
2022-01-30 19:10:06 +0900

    application.rbに許可ドメインを追加し、requestスペックテスト通過

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module Myapp
 
     # Don't generate system test files.
     config.generators.system_tests = nil
+    config.hosts << '.example.com'
   end
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,9 +2,13 @@ require 'rails_helper'
 
 RSpec.describe "StaticPages", type: :request do
   describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_index_path
-      expect(response).to have_http_status(200)
+    it "homeアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get static_pages_home_path
+      expect(response.status).to eq 200
+    end
+    it "helpアクションにリクエストすると正常にレスポンスが返ってくる" do
+      get static_pages_help_path
+      expect(response.status).to eq 200
     end
   end
 end
