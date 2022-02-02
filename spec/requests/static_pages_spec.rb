4b65087b14720f8f447bf3a4363e4ed1592515c5
nittamatama <blackmnshin@gmail.com>
2022-02-02 22:19:12 +0900

    各アクションにhtml要素有無のテストを追加

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -5,14 +5,17 @@ RSpec.describe "StaticPages", type: :request do
     it "homeアクションにリクエストすると正常にレスポンスが返ってくる" do
       get static_pages_home_path
       expect(response.status).to eq 200
+      expect(response.body).to include("title", "Home | Ruby on Rails Tutorial Sample App")
     end
     it "helpアクションにリクエストすると正常にレスポンスが返ってくる" do
       get static_pages_help_path
       expect(response.status).to eq 200
+      expect(response.body).to include("title", "Help | Ruby on Rails Tutorial Sample App")
     end
     it "aboutアクションにリクエストすると正常にレスポンスが返ってくる" do
       get static_pages_about_path
       expect(response.status).to eq 200
+      expect(response.body).to include("title", "About | Ruby on Rails Tutorial Sample App")
     end
   end
 end
